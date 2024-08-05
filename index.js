644101c8021f353303c72360c144dfa88586fa28
Jonas Bastisch <jonas.bastisch@gmail.com>
2024-08-05 14:26:15 +0200

    update logic

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 import { GraphQLClient, gql } from "graphql-request";
 import { ActionCable } from "@sorare/actioncable";
+import chalk from "chalk";
 import config from "./config.json" assert {type: 'json'};
 
 const API_KEY = 'c3a68bc9e201bd3f0150c7ecd9cdeb3f997d8415750207ec36acc55ae9e6ba9fdea08ff136fe60c2a8b6b491f699f783f212dbe2819d144b80d97181b95sr128';
-const SALES_TO_COMPARE = 20;
+const SALES_TO_COMPARE = 5;
 const PERCENTAGE_TO_CHECK = 80.0;
 
 const Subscription = gql`
-aCardWasUpdated(events: [offer_event_accepted, auction_event_closed]) 
+aCardWasUpdated(events: [offer_event_accepted]) 
 { 
     slug,
     rarity,
     averageScore(type: LAST_FIVE_SO5_AVERAGE_SCORE),
-    token,
+    inSeasonEligible,
+    player {
+      lastFifteenSo5Appearances,
+      lastFiveSo5Appearances,
+      position
+    }
+    tokenOwner,
     { 
-        owner 
-        { 
-            price
-            {
-                eur
-            } 
-        }
+      amounts
+      {
+          eur
+      } 
     } 
 }
 `;
+
+const error = chalk.bold.red;
+const player = chalk.bold.blue;
+const money = chalk.greenBright;
 
 async function startCheck() {
   const cable = new ActionCable({
@@ -58,14 +66,44 @@ function handleData(data) {
     return;
   }
 
-  const owner = data["result"]["data"]["aCardWasUpdated"]["token"]["owner"];
+  const owner = data["result"]["data"]["aCardWasUpdated"]["tokenOwner"];
   if (!owner) {
+    console.log(error("No owner found!"))
     return;
   }
 
+  const amounts = data["result"]["data"]["aCardWasUpdated"]["tokenOwner"]["amounts"];
+  if (!amounts) {
+    console.log(error("No price found!"))
+    //console.log(data["result"])
+    return;
+  }
 
-  const price = data["result"]["data"]["aCardWasUpdated"]["token"]["owner"]["price"]["eur"] / 100.00;
+  const price = data["result"]["data"]["aCardWasUpdated"]["tokenOwner"]["amounts"]["eur"] / 100.00;
   if (!price) {
+    console.log(error("No price found!"))
+    //console.log(data["result"])
+    return;
+  }
+
+  const rarity = data["result"]["data"]["aCardWasUpdated"]["rarity"];
+  console.log(rarity)
+
+  const position = data["result"]["data"]["aCardWasUpdated"]["player"]["position"]
+  if (!config["data"][rarity]["positions"].includes(position)) {
+    console.log(error("Position passt nicht"))
+    return;
+  }
+
+  const inSeason = data["result"]["data"]["aCardWasUpdated"]["inSeasonEligible"]
+  if (!config["data"][rarity]["inSeason"] === inSeason) {
+    console.log(error("Season passt nicht"))
+    return;
+  }
+
+  const lastAppearances = data["result"]["data"]["aCardWasUpdated"]["player"]["lastFifteenSo5Appearances"]
+  if (lastAppearances <= config["data"][rarity]["last15SO5Appearances"]) {
+    console.log(error("Apperances passt nicht"))
     return;
   }
 
@@ -75,8 +113,6 @@ function handleData(data) {
 
   //create only player slug
   const cleanedSlug = splittedSlug.slice(0, slugLength - 3).join("-");
-  const rarity = data["result"]["data"]["aCardWasUpdated"]["rarity"];
-  console.log(rarity);
 
   queryLastPrices(rarity, cleanedSlug, price, slug);
 }
@@ -92,24 +128,30 @@ async function queryLastPrices(rarity, playerSlug, priceSold, wholeSlug) {
   const priceArray = data["tokens"]["tokenPrices"];
 
   const pricesInEuro = priceArray.map((item) => item["amounts"]["eur"] / 100.00);
+  console.log(pricesInEuro)
 
   const priceCount = pricesInEuro.length;
+  console.log(priceCount)
+
   const averagePrice = pricesInEuro.reduce(
     (previous, current) => previous + current
     , 0.0) / priceCount;
   const percentageOfAverage = (priceSold / averagePrice) * 100;
 
-  if (percentageOfAverage > PERCENTAGE_TO_CHECK) return;
+  if (percentageOfAverage > PERCENTAGE_TO_CHECK) {
+    console.log(error("Price not over threshhold!"))
+    return;
+  }
 
   const curDate = new Date();
   const datetime = `[${curDate.getDate()}/${curDate.getMonth() + 1}/${curDate.getFullYear()} ${curDate.getHours()}:${curDate.getMinutes()}:${curDate.getSeconds()}]`;
 
   console.log(datetime);
-  console.log(wholeSlug);
-  console.log(`Paid price: ${priceSold.toFixed(2)}€`)
-  console.log(`Average Price: ${averagePrice.toFixed(2)}€`);
-  console.log(`Percentage of sale: ${percentageOfAverage.toFixed(2)}%`);
-  console.log(`Link: https://sorare.com/football/cards/${wholeSlug}`);
+  console.log(player(wholeSlug));
+  console.log(`Paid price: ${money(priceSold.toFixed(2))}€`)
+  console.log(`Average Price: ${money(averagePrice.toFixed(2))}€`);
+  console.log(`Percentage of sale: ${money(percentageOfAverage.toFixed(2))}%`);
+  console.log(`Link: ${chalk.blueBright(`https://sorare.com/football/cards/${wholeSlug}`)}`);
   console.log("--------------------------------------------------------------------------");
 }
 
@@ -117,7 +159,7 @@ function getPriceQuery(rarity, playerSlug) {
   return gql`
     query LastPrices {
       tokens {
-        tokenPrices(first: ${SALES_TO_COMPARE}, collection: FOOTBALL, rarity: ${rarity}, playerSlug: "${playerSlug}") {
+        tokenPrices(first: ${SALES_TO_COMPARE}, rarity: ${rarity}, playerSlug: "${playerSlug}", seasonEligibility: CLASSIC) {
           amounts {
             eur
           }
